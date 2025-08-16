1113f8e5d570136001ff87fd751e88d2e0c4e9ec
anuroopdhaliwal <u3306135@uni.canberra.edu.au>
2025-08-17 09:29:40 +1000

    Update wordcode.py

--- step4wordcode/wordcode.py
+++ step4wordcode/wordcode.py
@@ -1,19 +1,19 @@
 Word Code:
-1.	Input the scheduled feeding times.
+1.	Input the scheduled feeding times. #user input
 2.	For each scheduled feeding time:
 IF (TIME /= SCHEDULEDTIME)
 THEN 
 	 Wait 10 minutes.
 	Check again.
-ELSE IF (TIME = SCHEDULE TIME)
+ELSE IF (TIME = SCHEDULEDTIME)
 THEN 
 	Activate MOTOR and dispense food.
 3.	Check if food was dispensed:
-IF (FOODDISPENSED = FALSE)
+IF (FOODDISPENSED = FALSE) #food not detected by sensor after motor activated
 	ALERT = 1
 	UPDATE = 0
 	End the feeding cycle.
-ELSE IF (FOODDISPENSED = TRUE)
+ELSE IF (FOODDISPENSED = TRUE) #food is detected by sensor after motor activated
 	Continue.
 4.	Check if feeding period passed:
 IF FEEDINGPERIOD has not passed 
@@ -22,10 +22,10 @@ IF FEEDINGPERIOD has not passed
 ELSE IF FEEDINGPERIOD has passed
 	Continue.
 5.	Check if food was consumed:
-IF (FOODREMAINING = FALSE)
+IF (FOODREMAINING = FALSE) #the food has been consumed
 	ALERT = 0
 	UPDATE = 1
-ELSE IF (FOODREMAINING = TRUE)
+ELSE IF (FOODREMAINING = TRUE) #the food has not been consumed
 	ALERT = 1
 	UPDATE = 0
 6.	Repeat for all scheduled feeding times.
